4eeb9cb24566ce2fa99724c66f6a08613b4f6580
Cody James <xiejx5@gmail.com>
2025-03-31 22:51:16 +0200

    add cartopy as dependency

--- geoplots/cartopy.py
+++ geoplots/cartopy.py
@@ -1,7 +1,3 @@
-from osgeo import osr
-import cartopy.crs as ccrs
-
-
 def cartopy_crs(ds):
     """Converts a pyproj.Proj to a cartopy.crs.Projection
     Parameters
@@ -12,29 +8,35 @@ def cartopy_crs(ds):
     -------
     a cartopy.crs.Projection object
     """
+    from osgeo import osr
+    import cartopy.crs as ccrs
+
     proj = ds.GetProjection()
     inproj = osr.SpatialReference()
     inproj.ImportFromWkt(proj)
     srs = inproj.ExportToProj4()
 
-    km_proj = {'lon_0': 'central_longitude',
-               'lat_0': 'central_latitude',
-               'x_0': 'false_easting',
-               'y_0': 'false_northing',
-               'k': 'scale_factor',
-               'zone': 'zone',
-               }
-    km_globe = {'a': 'semimajor_axis',
-                'b': 'semiminor_axis',
-                }
-    km_std = {'lat_1': 'lat_1',
-              'lat_2': 'lat_2',
-              }
+    km_proj = {
+        "lon_0": "central_longitude",
+        "lat_0": "central_latitude",
+        "x_0": "false_easting",
+        "y_0": "false_northing",
+        "k": "scale_factor",
+        "zone": "zone",
+    }
+    km_globe = {
+        "a": "semimajor_axis",
+        "b": "semiminor_axis",
+    }
+    km_std = {
+        "lat_1": "lat_1",
+        "lat_2": "lat_2",
+    }
     kw_proj = dict()
     kw_globe = dict()
     kw_std = dict()
-    for s in srs.split('+'):
-        s = s.split('=')
+    for s in srs.split("+"):
+        s = s.split("=")
         if len(s) != 2:
             continue
         k = s[0].strip()
@@ -43,16 +45,16 @@ def cartopy_crs(ds):
             v = float(v)
         except BaseException:
             pass
-        if k == 'proj':
-            if v == 'tmerc':
+        if k == "proj":
+            if v == "tmerc":
                 pycl = ccrs.TransverseMercator
-            if v == 'lcc':
+            if v == "lcc":
                 pycl = ccrs.LambertConformal
-            if v == 'merc':
+            if v == "merc":
                 pycl = ccrs.Mercator
-            if v == 'utm':
+            if v == "utm":
                 pycl = ccrs.UTM
-            if v == 'aea':
+            if v == "aea":
                 pycl = ccrs.AlbersEqualArea
         if k in km_proj:
             kw_proj[km_proj[k]] = v
@@ -65,11 +67,11 @@ def cartopy_crs(ds):
     if kw_globe:
         globe = ccrs.Globe(**kw_globe)
     if kw_std:
-        kw_proj['standard_parallels'] = (kw_std['lat_1'], kw_std['lat_2'])
+        kw_proj["standard_parallels"] = (kw_std["lat_1"], kw_std["lat_2"])
 
     # mercatoooor
-    if pycl.__name__ == 'Mercator':
-        kw_proj.pop('false_easting', None)
-        kw_proj.pop('false_northing', None)
+    if pycl.__name__ == "Mercator":
+        kw_proj.pop("false_easting", None)
+        kw_proj.pop("false_northing", None)
 
     return pycl(globe=globe, **kw_proj)
