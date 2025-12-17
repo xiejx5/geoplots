42e801c200ae830f848d0a55f1e4584d78bc3142
Cody James <xiejx5@gmail.com>
2025-12-17 15:09:46 +0100

    feat: Migrate to `pyproject.toml`, add Roboto fonts

--- geoplots/__init__.py
+++ geoplots/__init__.py
@@ -1,3 +1,11 @@
+import os
+import matplotlib.font_manager as fm
+
+_font_dir = os.path.join(os.path.dirname(__file__), 'fonts')
+for font_file in os.listdir(_font_dir):
+    if font_file.endswith('.ttf'):
+        fm.fontManager.addfont(os.path.join(_font_dir, font_file))
+
 from geoplots._const import *
 from geoplots.wrapper import *
 from geoplots.cartopy import *
